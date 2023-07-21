536fc6587384a60c3b8cf70efe7ffa4c169f8aa8
Duncc19 <dunncc199@gmail.com>
2023-07-21 08:39:55 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Jojo-renew',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
